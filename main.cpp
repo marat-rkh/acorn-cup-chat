c2520007cffa54b76a0dc41c52749d6e3a5a3533
marat-rkh <marat.r.khabibullin@gmail.com>
2025-09-21 12:33:42 +0300

    Add init and fix linking

--- main.cpp
+++ main.cpp
@@ -4,6 +4,20 @@
 using namespace std;
 
 int main() {
+    PaError paErr = Pa_Initialize();
+    if (paErr != paNoError) {
+        cerr << "Pa_Initialize error: " << Pa_GetErrorText(paErr) << endl;
+        return 1;
+    }
+
     cout << "Hello world!" << endl;
+
+    // TODO Use RAII
+    paErr = Pa_Terminate();
+    if (paErr != paNoError) {
+        cerr << "Pa_Terminate error: " << Pa_GetErrorText(paErr) << endl;
+        return 1;
+    }
+
     return 0;
 }
